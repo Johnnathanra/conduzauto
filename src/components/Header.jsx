433569eb577b7c5e1c7b3a559ecab5bd1375d3dd
Johnnathanra <johndesigner82@gmail.com>
2026-02-26 09:40:39 -0300

    Remove unused useState import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../contexts/ThemeContext';
 import { Sun, Moon, LogIn } from 'lucide-react';
