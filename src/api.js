e93cb20b0f812c911d0545c36d175cd538fee9f1
Johnnathanra <johndesigner82@gmail.com>
2026-02-26 22:50:37 -0300

    Update API URL to Railway production backend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://conduzauto-production.up.railway.app/api';
+const API_URL = 'https://conduzauto-backend-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
